faf2d164446b8ba990d87b23b7bb6aa3dec83be7
Trilowy <49493635+trilowy@users.noreply.github.com>
2025-11-24 19:37:33 +0100

    Format code with zig fmt.

--- build.zig
+++ build.zig
@@ -1,32 +1,32 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-	const target = b.standardTargetOptions(.{});
-	const optimize = b.standardOptimizeOption(.{});
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
 
-	// Add anyascii dependency.
-	const anyascii = b.dependency("anyascii", .{
-		.target = target,
-		.optimize = optimize,
-	});
+    // Add anyascii dependency.
+    const anyascii = b.dependency("anyascii", .{
+        .target = target,
+        .optimize = optimize,
+    });
 
-	// Zlugify zig module.
-	const zlugify = b.addModule("zlugify", .{
-		.root_source_file = b.path("src/lib.zig"),
-		.target = target,
-		.optimize = optimize,
-	});
-	// Add anyascii dependency.
-	zlugify.addImport("anyascii", anyascii.module("anyascii"));
+    // Zlugify zig module.
+    const zlugify = b.addModule("zlugify", .{
+        .root_source_file = b.path("src/lib.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    // Add anyascii dependency.
+    zlugify.addImport("anyascii", anyascii.module("anyascii"));
 
-	// Library unit tests.
-	const lib_unit_tests = b.addTest(.{
-		.root_module = zlugify,
-		.target = target,
-		.optimize = optimize,
-	});
-	const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
+    // Library unit tests.
+    const lib_unit_tests = b.addTest(.{
+        .root_module = zlugify,
+        .target = target,
+        .optimize = optimize,
+    });
+    const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
 
-	const test_step = b.step("test", "Run unit tests.");
-	test_step.dependOn(&run_lib_unit_tests.step);
+    const test_step = b.step("test", "Run unit tests.");
+    test_step.dependOn(&run_lib_unit_tests.step);
 }
--- src/lib.zig
+++ src/lib.zig
@@ -7,90 +7,90 @@ const valuesToStrip = " \t\r\n\'\"/\\";
 /// Convert the provided string to a slugged version of it.
 /// With this function, you can set the separator to use.
 pub fn slugifySeparator(allocator: std.mem.Allocator, str: []const u8, separator: u8) ![]u8 {
-	// Convert the provided UTF-8 string to ASCII.
-	const fullResult = try anyascii.utf8ToAscii(allocator, str);
-	const startShift = fullResult.len - std.mem.trimLeft(u8, fullResult, valuesToStrip).len;
-	const endShift = fullResult.len - std.mem.trimRight(u8, fullResult, valuesToStrip).len;
-	const result = fullResult[startShift..fullResult.len - endShift];
+    // Convert the provided UTF-8 string to ASCII.
+    const fullResult = try anyascii.utf8ToAscii(allocator, str);
+    const startShift = fullResult.len - std.mem.trimLeft(u8, fullResult, valuesToStrip).len;
+    const endShift = fullResult.len - std.mem.trimRight(u8, fullResult, valuesToStrip).len;
+    const result = fullResult[startShift .. fullResult.len - endShift];
 
-	// Check each char to remove them / replace them by their slugged version if needed.
-	var previousIsSeparator = true; // Setting it to true at start forbids the result to start with a separator.
-	var shift: usize = 0;
-	for (0..result.len, result) |i, char| {
-		if (char == ' ' or char == '\t' or char == '\r' or char == '\n' or char == '\'' or char == '"' or char == '/' or char == '\\') {
-			// Whitespace-like character: replace it by a dash, or remove it if the previous character is a dash.
-			if (!previousIsSeparator) {
-				fullResult[i - shift] = separator;
-				previousIsSeparator = true;
-			} else {
-				// To remove the current character, we just shift all future written characters.
-				shift += 1;
-			}
-		} else {
-			// In the general case, we keep alphanumeric characters and all the rest is shifted.
-			if (std.ascii.isAlphanumeric(char)) {
-				// Convert the ASCII character to its lowercased version.
-				fullResult[i - shift] = std.ascii.toLower(char);
-				previousIsSeparator = false;
-			} else {
-				shift += 1;
-			}
-		}
-	}
+    // Check each char to remove them / replace them by their slugged version if needed.
+    var previousIsSeparator = true; // Setting it to true at start forbids the result to start with a separator.
+    var shift: usize = 0;
+    for (0..result.len, result) |i, char| {
+        if (char == ' ' or char == '\t' or char == '\r' or char == '\n' or char == '\'' or char == '"' or char == '/' or char == '\\') {
+            // Whitespace-like character: replace it by a dash, or remove it if the previous character is a dash.
+            if (!previousIsSeparator) {
+                fullResult[i - shift] = separator;
+                previousIsSeparator = true;
+            } else {
+                // To remove the current character, we just shift all future written characters.
+                shift += 1;
+            }
+        } else {
+            // In the general case, we keep alphanumeric characters and all the rest is shifted.
+            if (std.ascii.isAlphanumeric(char)) {
+                // Convert the ASCII character to its lowercased version.
+                fullResult[i - shift] = std.ascii.toLower(char);
+                previousIsSeparator = false;
+            } else {
+                shift += 1;
+            }
+        }
+    }
 
-	// If we removed characters, free the remaining unused memory.
-	if (shift > 0 or startShift > 0 or endShift > 0) {
-		if (!allocator.resize(fullResult, result.len - shift)) {
-			// In case of a failed resize, reallocate.
-			defer allocator.free(fullResult);
-			const resultAlloc = try allocator.alloc(u8, result.len - shift);
-			@memcpy(resultAlloc, fullResult[0..result.len - shift]);
-			return resultAlloc;
-		}
-	}
+    // If we removed characters, free the remaining unused memory.
+    if (shift > 0 or startShift > 0 or endShift > 0) {
+        if (!allocator.resize(fullResult, result.len - shift)) {
+            // In case of a failed resize, reallocate.
+            defer allocator.free(fullResult);
+            const resultAlloc = try allocator.alloc(u8, result.len - shift);
+            @memcpy(resultAlloc, fullResult[0 .. result.len - shift]);
+            return resultAlloc;
+        }
+    }
 
-	// Return the result without the shifted characters.
-	return fullResult[0..result.len - shift];
+    // Return the result without the shifted characters.
+    return fullResult[0 .. result.len - shift];
 }
 
 /// Convert the provided string to a slugged version of it with the default '-' separator.
 pub fn slugify(allocator: std.mem.Allocator, str: []const u8) ![]u8 {
-	return slugifySeparator(allocator, str, '-');
+    return slugifySeparator(allocator, str, '-');
 }
 
 test slugify {
-	try testSlugify("this-is-a-test", "   This is a test.\t\n");
-	try testSlugify("something-else", "SôMÈThing   \t    ÉLSÈ");
-	try testSlugify("slugify-a-string", "𝒔𝒍𝒖𝒈𝒊𝒇𝒚 𝒂 𝒔𝒕𝒓𝒊𝒏𝒈");
-	try testSlugify("a", "à ");
+    try testSlugify("this-is-a-test", "   This is a test.\t\n");
+    try testSlugify("something-else", "SôMÈThing   \t    ÉLSÈ");
+    try testSlugify("slugify-a-string", "𝒔𝒍𝒖𝒈𝒊𝒇𝒚 𝒂 𝒔𝒕𝒓𝒊𝒏𝒈");
+    try testSlugify("a", "à ");
 
-	try testSlugify("blosse-shenzhen", "Blöße 深圳");
-	try testSlugify("qiyu-xian", "埼玉 県");
-	try testSlugify("samt-redia", "სამტრედია");
-	try testSlugify("say-x-ag", "⠠⠎⠁⠽⠀⠭⠀⠁⠛");
-	try testSlugify("5-x", "☆ ♯ ♰ ⚄ ⛌");
-	try testSlugify("no-m-a-s", "№ ℳ ⅋ ⅍");
+    try testSlugify("blosse-shenzhen", "Blöße 深圳");
+    try testSlugify("qiyu-xian", "埼玉 県");
+    try testSlugify("samt-redia", "სამტრედია");
+    try testSlugify("say-x-ag", "⠠⠎⠁⠽⠀⠭⠀⠁⠛");
+    try testSlugify("5-x", "☆ ♯ ♰ ⚄ ⛌");
+    try testSlugify("no-m-a-s", "№ ℳ ⅋ ⅍");
 
-	try testSlugify("hearts", "♥");
-	try testSlugify("hello-fox", "hello 🦊");
-	try testSlugify("deja-vu", "  Déjà Vu!  ");
-	try testSlugify("toi-yeu-nhung-chu-ky-lan", "tôi yêu những chú kỳ lân");
+    try testSlugify("hearts", "♥");
+    try testSlugify("hello-fox", "hello 🦊");
+    try testSlugify("deja-vu", "  Déjà Vu!  ");
+    try testSlugify("toi-yeu-nhung-chu-ky-lan", "tôi yêu những chú kỳ lân");
 }
 /// Test slugify function.
 fn testSlugify(expected: []const u8, toSlugify: []const u8) !void {
-	const slug = try slugify(std.testing.allocator, toSlugify);
-	defer std.testing.allocator.free(slug);
+    const slug = try slugify(std.testing.allocator, toSlugify);
+    defer std.testing.allocator.free(slug);
 
-	try std.testing.expectEqualStrings(expected, slug);
+    try std.testing.expectEqualStrings(expected, slug);
 }
 
 test slugifySeparator {
-	try testSlugifySeparator("something_else", "SôMÈThing   \t    ÉLSÈ", '_');
+    try testSlugifySeparator("something_else", "SôMÈThing   \t    ÉLSÈ", '_');
 }
 /// Test slugifySeparator function.
 fn testSlugifySeparator(expected: []const u8, toSlugify: []const u8, separator: u8) !void {
-	const slug = try slugifySeparator(std.testing.allocator, toSlugify, separator);
-	defer std.testing.allocator.free(slug);
+    const slug = try slugifySeparator(std.testing.allocator, toSlugify, separator);
+    defer std.testing.allocator.free(slug);
 
-	try std.testing.expectEqualStrings(expected, slug);
+    try std.testing.expectEqualStrings(expected, slug);
 }
